40ae60174abfd766bd09044d288747e9b93ff74c
shehan774690541 <shehan774690541@gmail.com>
2025-04-13 20:55:47 +0530

    .

--- admin.php
+++ admin.php
@@ -1,8 +1,25 @@
 <?php
-    $permission = false;
+    $permission = true;
     if ($permission) {
-        echo "Permission Allowed";
+        
+        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https" : "http";
+        $host = $_SERVER['HTTP_HOST'];
+        $path = $_SERVER['REQUEST_URI'];
+
+        $fullUrl = $protocol . "://" . $host . $path;
+
+        $parts = explode("/", trim($path, "/"));
+
+        echo "paths : <br />";
+        for ($i=0; $i < 2; $i++) {
+            echo $parts[$i] . "<br />";
+        }
+        echo "===============<br />";
+
+        // Echo it
+        echo "💻 Your current path is: <strong>$fullUrl</strong>";
     }else{
-        require '404.php';
+        header("Location: /404.php");
+        exit;
     }
 ?>
